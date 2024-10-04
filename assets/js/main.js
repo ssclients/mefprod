b3503838ea41af8bf75183560018ba37b5b228e8
SS Clients <rockstar250802@gmail.com>
2024-10-04 16:16:36 +0530

    26 commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,21 +133,7 @@
     $('#header').addClass('header-scrolled');
   }
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
 
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
 
   // Initiate the venobox plugin
   $(window).on('load', function() {
